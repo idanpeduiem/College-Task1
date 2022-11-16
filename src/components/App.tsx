cce6ed093d4121ae81500fa0d8dc016f31ba1afe
avivtsadik <48044366+avivtsadik@users.noreply.github.com>
2022-11-16 19:48:55 +0200

    item component
    item component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,15 @@
 import React from "react";
 import { Paper, Typography } from "@mui/material";
+import { Item } from "./Item";
 
 export const App = () => {
-  return <Typography variant="h1">This is your first app!</Typography>;
+  return (
+    <>
+      <Item
+        price={1}
+        imagePrimary="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
+        productName={"aviv"}
+      ></Item>
+    </>
+  );
 };
