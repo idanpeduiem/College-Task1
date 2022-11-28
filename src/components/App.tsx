c4b0d321f10e4b77cfee952e94e96ea4fdff5a4b
idanpeduiem <62710926+idanpeduiem@users.noreply.github.com>
2022-11-28 20:46:49 +0200

    add title to page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { ProductCard } from "@components/ProductCard";
 import { products, ProductType } from "@services/productsManager";
-import { Dialog, Grid } from "@mui/material";
+import {Grid, Typography} from "@mui/material";
 import { grey } from "@mui/material/colors";
 import { ProductDialog } from "./ProductDialog";
 
@@ -15,20 +15,23 @@ const App = () => {
   };
 
   return (
-    <Grid container sx={{ bgcolor: grey["400"] }} spacing={10}>
-      {products.map((product: ProductType, key) => (
-        <Grid key={key} item xl={3} md={4} xs={6}>
-          <ProductCard product={product} onShowMore={onItemSelected} />
-        </Grid>
-      ))}
-      <ProductDialog
-        fullWidth
-        maxWidth="sm"
-        product={productSelected}
-        open={isDialogOpen}
-        onClose={() => setIsDialogOpen(false)}
-      ></ProductDialog>
-    </Grid>
+    <>
+      <Typography variant='h2'>Idan And Aviv Store</Typography>
+      <Grid container sx={{ bgcolor: grey["400"] }} spacing={10}>
+        {products.map((product: ProductType, key) => (
+          <Grid key={key} item xl={3} md={4} xs={6}>
+            <ProductCard product={product} onShowMore={onItemSelected} />
+          </Grid>
+        ))}
+        <ProductDialog
+          fullWidth
+          maxWidth="sm"
+          product={productSelected}
+          open={isDialogOpen}
+          onClose={() => setIsDialogOpen(false)}
+        ></ProductDialog>
+      </Grid>
+    </>
   );
 };
 
